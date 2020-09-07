334c35b34fe229a2f289d42c6d05d7b07adea4e9
AnishTiwari <tiwari1999anish@gmail.com>
2020-09-08 01:48:58 +0530

    Updated scripts

--- scholarship_app/Content/db.sql
+++ scholarship_app/Content/db.sql
@@ -43,3 +43,43 @@ REFERENCES [dbo].[student] ([Id])
 GO
 ALTER TABLE [dbo].[scholarship_details] CHECK CONSTRAINT [FK_scholarship_details]
 GO
+
+
+// updated
+
+alter table student add PhoneNumber varchar(11), Password varchar(50);
+
+create table listings(
+
+Id bigint primary key identity(1,1) not null,
+GivenBy varchar(200),
+CreatedTime DateTime,
+Amount int ,
+Currency varchar(5),
+EndDate Date,
+NoAvailable bigint,
+);
+
+alter table listings add student_id bigint;
+
+ALTER TABLE [dbo].listings  WITH CHECK ADD  CONSTRAINT [FK_slistings] FOREIGN KEY([student_id])
+REFERENCES [dbo].[student] ([Id])
+GO
+
+
+CREATE TABLE [dbo].availed (
+    -- Optionally, we can give the table its own surrogate PK
+    [Id]      BigINT IDENTITY(1,1) NOT NULL,
+    student_id BigINT NOT NULL,
+    listing_id BigINT NOT NULL,
+
+    -- Referential Integrity
+    FOREIGN KEY(student_id) REFERENCES Student(Id),
+    FOREIGN KEY(listing_id) REFERENCES listings(Id),
+
+    -- PK is either the surrogate ...
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+    -- ... Or the compound key
+    -- PRIMARY KEY CLUSTERED (AuthorId, BookId)
+);
+
